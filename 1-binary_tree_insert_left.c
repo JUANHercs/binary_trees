156c618d27ba8fc2b8d65bea6b29815b54188a09
Brian Zapata <dzapata397@gmail.com>
2021-07-20 22:35:19 +0000

    True Output

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,23 +5,23 @@
  *@parent: apointer to the parent of the node to create.
  *@value: the value of the new node.
  *
- *
- *
+ *Return: If an error occurs - Null.
+ * other wise -pointer to new nde.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL, *temp = NULL;
+	binary_tree_t *new_node = NULL;
 
 	if (parent == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(binary_tree_t));
+		return NULL;
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
-	temp = parent;
-	new_node->n = value, new_node->parent = parent,
-	new_node->left = NULL, new_node->right = NULL;
-	if (temp->left != NULL)
-		new_node->left = temp->left,
-    	temp->left = new_node;
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new_node;
+		new_node->left = parent->left;
+	}
+	parent->left = new_node;
 	return (new_node);
 }
