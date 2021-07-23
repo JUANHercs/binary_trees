888f107d63fe4118da1c0b15ff365c4d2350d4eb
natyarteagac <natalia.arteagac94@gmail.com>
2021-07-22 20:27:41 -0500

    Fixing checker

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,17 +7,14 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	binary_tree_t *left = NULL, *right = NULL;
 
-	left = tree->left;
-	right = tree->right;
 	if (tree == NULL)
 		return (0);
 	if (binary_tree_is_leaf(tree))
 		return (1);
-	if (binary_tree_height(left) == binary_tree_height(right))
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 	{
-		if (binary_tree_is_perfect(left) && binary_tree_is_perfect(right))
+		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 	return (0);
