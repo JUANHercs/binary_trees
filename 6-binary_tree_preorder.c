f02b042f2e771bafff241ccf7280da95c2c5abe0
natyarteagac <natalia.arteagac94@gmail.com>
2021-07-31 14:12:56 -0500

    Cleaning repository

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,6 +11,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (func == NULL || tree == NULL)
 		return;
 	func(tree->n);
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
